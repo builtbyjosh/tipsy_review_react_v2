e210932ce857beb493717783694724329af59d5d
builtbyjosh <pc.refuge@gmail.com>
2021-05-13 19:24:42 -0500

    completed basic layout of components

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import BreweryContainer from './BreweryContainer';
+import React, { Component } from 'react'
+import BreweryContainer from "./BreweryContainer";
 
-
-const App = () => {
-  return (
-    <div className="ui container">
-      <BreweryContainer />
-    </div>
-  );
+ class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className='ui container'>Brewery Container - eventually going to be nav bar</h1>
+      <hr />
+      <div className="ui container grid">
+        <BreweryContainer />
+      </div>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
--- src/components/BreweryContainer.js
+++ src/components/BreweryContainer.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react'
-import BreweryDetails from './BreweryDetails'
-import BreweryList from './BreweryList'
+import React, { Component } from "react";
+import BreweryDetails from "./BreweryDetails";
+import BreweryList from "./BreweryList";
 
 export default class BreweryContainer extends Component {
   render() {
     return (
-      <div>
-        <h1>Container</h1>
-        <BreweryList />
-        <BreweryDetails />
+      <div className="ui row">
+        
+        <div className="column eight wide">
+          <BreweryList />
+        </div>
+        <div className="column eight wide">
+          <BreweryDetails />
+        </div>
       </div>
-    )
+    );
   }
 }

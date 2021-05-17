70e682fff3835a48b5af8e0db0ea7cbce07ef918
builtbyjosh <pc.refuge@gmail.com>
2021-05-17 14:11:40 -0500

    refactor for conditional display

--- src/components/BreweryList.js
+++ src/components/BreweryList.js
@@ -4,28 +4,37 @@ import { selectedBrewery } from "../redux/actions/";
 
 class BreweryList extends Component {
   render() {
-    return (
-      <>
-        {this.props.breweries.map((brewery) => {
-          return (
-            <div className="item" key={brewery.id}>
-              <div
-                className="content"
-                onClick={(e) => {
-                  this.props.selectedBrewery(brewery);
-                }}
-              >
-                <p className="ui header">{brewery.name}</p>
-              </div>
-            </div>
-          );
-        })}
-      </>
-    );
+    {
+      if (this.props.breweries.length > 0) {
+        return (
+          <div>
+            <h1>Search Results: {this.props.breweries.length}</h1>
+            {this.props.breweries.map((brewery) => {
+              return (
+                <div className="item" key={brewery.id}>
+                  <div
+                    className="content"
+                    onClick={(e) => {
+                      this.props.selectedBrewery(brewery);
+                    }}
+                  >
+                    <p className="ui header">{brewery.name}</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        );
+      }
+    }
+    return <></>
   }
 }
+
 const mapStateToProps = (state) => {
   return { breweries: state.breweries };
 };
 
 export default connect(mapStateToProps, { selectedBrewery })(BreweryList);
+
+

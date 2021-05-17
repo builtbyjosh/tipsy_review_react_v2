a65e05821b1fb0d9e50432b2a2b4e0cf91cc6cbf
builtbyjosh <pc.refuge@gmail.com>
2021-05-17 10:47:10 -0500

    created rating dropdown menu

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -26,11 +26,27 @@ class ReviewForm extends Component {
               name="comment"
               id="review-comment"
               cols="30"
-              rows="10"
+              rows="4"
             ></textarea>
           </div>
-          <div className="ui star rating" data-rating="5"></div>
-          <button type="submit">Submit Rating</button>
+          <div className="field">
+            <label>Rating: </label>
+            <select className="ui dropdown">
+              <option value="" >1-10</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <button type="submit">Add Review</button>
         </form>
       </>
     );

5140919740ab21b87f59b696f12ba72f2cfdeb24
builtbyjosh <pc.refuge@gmail.com>
2021-05-17 09:43:56 -0500

    Configured display of brewery list and details

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 import { fetchAPIBreweries, selectedBrewery } from "../redux/actions/";
-
+import BreweryDetails from "./BreweryDetails";
 
 class SearchContainer extends Component {
   componentDidMount() {
@@ -11,14 +11,32 @@ class SearchContainer extends Component {
 
   render() {
     return (
-      <div>
+
         <div className="ui row">
           <div className="column six wide">
-            <h1 className="ui header">Search Results</h1>
-            {console.log(this.props.breweries)}
+            <div className="ui divided list">
+              <h1 className="ui header">Search Results</h1>
+              {this.props.breweries.map((brewery) => {
+                return (
+                  <div className="item" key={brewery.id}>
+                    <div
+                      className="content"
+                      onClick={(e) => {
+                        this.props.selectedBrewery(brewery);
+                      }}
+                    >
+                      <p className="ui header">{brewery.name}</p>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="column ten wide">
+            <BreweryDetails />
           </div>
         </div>
-      </div>
+
     );
   }
 }

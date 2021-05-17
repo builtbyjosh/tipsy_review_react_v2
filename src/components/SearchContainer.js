70228b2d6b78b71167237c6738313a8a1a58729f
builtbyjosh <pc.refuge@gmail.com>
2021-05-17 13:42:47 -0500

    connected search bar to search container

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -4,16 +4,28 @@ import { connect } from "react-redux";
 import { fetchAPIBreweries, selectedBrewery } from "../redux/actions/";
 import BreweryDetails from "./BreweryDetails";
 import BreweryList from "./BreweryList";
+import SearchBar from "./SearchBar";
 
 class SearchContainer extends Component {
-  componentDidMount() {
-    this.props.fetchAPIBreweries();
+  // componentDidMount() {
+  //   this.props.onQuerySubmit('holland');
+  // }
+
+  onSearchSubmit = (query) => {
+    this.props.fetchAPIBreweries(query)
   }
 
   render() {
     return (
-
+      <>
         <div className="ui row">
+          <div className="column twelve wide">
+            <SearchBar onFormSubmit={this.onSearchSubmit}/>
+          </div>
+          
+        </div>
+        
+        <div className="ui row">          
           <div className="column six wide">
             <div className="ui divided list">
               <h1 className="ui header">Search Results</h1>
@@ -24,6 +36,8 @@ class SearchContainer extends Component {
             <BreweryDetails />
           </div>
         </div>
+      </>
+        
 
     );
   }

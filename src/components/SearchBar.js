4ed066ce745cc80df18139e8a842436eacb4c7d3
builtbyjosh <pc.refuge@gmail.com>
2021-05-17 13:39:26 -0500

    created search bar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  state = { query: "" };
+
+  onInputChange = (e) => {
+    this.setState({ query: e.target.value });
+  };
+
+  onFormSubmit = (e) => {
+    e.preventDefault()
+    this.props.onFormSubmit(this.state.query)
+    console.log(this.state.query)    
+  }
+
+  render() {
+    return (
+      <div className="ui segment search-bar">
+        <form action="" className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label>Search:</label>
+            <input
+              type="text"
+              value={this.state.query}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+export default SearchBar;

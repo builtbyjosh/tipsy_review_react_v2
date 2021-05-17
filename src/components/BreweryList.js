5b3453ac4b84654477046d4fbb23826212a073a0
builtbyjosh <pc.refuge@gmail.com>
2021-05-17 10:13:38 -0500

    refactored to a class component to be reusable

--- src/components/BreweryList.js
+++ src/components/BreweryList.js
@@ -1,25 +1,31 @@
-import React from "react";
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import { selectedBrewery } from "../redux/actions/";
 
-const BreweryList = ({ breweries }) => {
-  return breweries.map((brewery) => {
+class BreweryList extends Component {
+  render() {
     return (
-      <div className="item" key={brewery.id}>
-        <div
-          className="content"
-          onClick={() => {this.props.selectedBrewery(brewery); }}
-        >
-          <h3 className="ui header">{brewery.name}</h3>
-        </div>
-      </div>
+      <>
+        {this.props.breweries.map((brewery) => {
+          return (
+            <div className="item" key={brewery.id}>
+              <div
+                className="content"
+                onClick={(e) => {
+                  this.props.selectedBrewery(brewery);
+                }}
+              >
+                <p className="ui header">{brewery.name}</p>
+              </div>
+            </div>
+          );
+        })}
+      </>
     );
-  });
+  }
+}
+const mapStateToProps = (state) => {
+  return { breweries: state.breweries };
 };
 
-const mapStateToProps = state => {
-  return {breweries: state.breweries}
-}
-
-export default connect(mapStateToProps)(BreweryList);
-
+export default connect(mapStateToProps, { selectedBrewery })(BreweryList);

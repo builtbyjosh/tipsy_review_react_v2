9e4c2cbbbe0cf6dfe7bff9c4c289c8ab44249593
builtbyjosh <pc.refuge@gmail.com>
2021-05-18 20:45:39 -0500

    add brewerReviews action"

--- src/components/BreweryList.js
+++ src/components/BreweryList.js
@@ -1,33 +1,29 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { selectedBrewery } from "../redux/actions/";
+import { selectedBrewery, breweryReviews } from "../redux/actions/";
 
 class BreweryList extends Component {
   render() {
-    {
-      if (this.props.breweries.length > 0) {
-        return (
-          <>
-            <h2>Search Results: {this.props.breweries.length}</h2>
-            {this.props.breweries.map((brewery) => {
-              return (
-                <div className="item" key={brewery.id}>
-                  <div
-                    className="content"
-                    onClick={(e) => {
-                      this.props.selectedBrewery(brewery);
-                    }}
-                  >
-                    <p className="ui header">{brewery.name}</p>
-                  </div>
-                </div>
-              );
-            })}
-          </>
-        );
-      }
-    }
-    return <></>;
+    return (
+      <>
+        <h2>Search Results: {this.props.breweries.length}</h2>
+        {this.props.breweries.map((brewery) => {
+          return (
+            <div className="item" key={brewery.id}>
+              <div
+                className="content"
+                onClick={(e) => {
+                  this.props.selectedBrewery(brewery);
+                  this.props.breweryReviews(brewery.reviews);
+                }}
+              >
+                <p className="ui header">{brewery.name}</p>
+              </div>
+            </div>
+          );
+        })}
+      </>
+    );
   }
 }
 
@@ -35,4 +31,6 @@ const mapStateToProps = (state) => {
   return { breweries: state.breweries };
 };
 
-export default connect(mapStateToProps, { selectedBrewery })(BreweryList);
+export default connect(mapStateToProps, { selectedBrewery, breweryReviews })(
+  BreweryList
+);
